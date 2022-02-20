c636845772e625caabf6e99e7f6de793d17f27be
malkarie <malkarian@gmail.com>
2022-02-20 22:17:18 +1000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // METAMASK CONNECTION
 const TIMEOUT = 1000;
-const COLLECTION_NAME = 'CodeCats';
+const COLLECTION_NAME = 'KrazyKittenBeansKlub';
 let editions = [];
 let dots = 1;
 
@@ -72,24 +72,6 @@ const checkOwner = async (account) => {
 
     updateStatusText(isOwner, false)
   }
-}
-
-function updateStatusText(isOwner, checking) {
-  const statusText = document.querySelector('.owner-status');
-  if(checking) {
-    if(isOwner) {
-      statusText.innerText = `You do own ${COLLECTION_NAME}!! 😻 Let's see how many${renderDots(dots)}`;
-    } else {
-      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} 😻${renderDots(dots)}`;
-    }
-  } else {
-    if(isOwner) {
-      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! 😻`;
-    } else {
-      statusText.innerText = `You don't own any ${COLLECTION_NAME} 😿`;
-    }
-  }
-  dots = dots === 3 ? 1 : dots + 1;
 }
 
 function renderDots(dots) {
